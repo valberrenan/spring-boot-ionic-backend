75734f20099aac7b994382b9863872021a5b994a
“Valber” <valber.lrenan@mercadolivre.com>
2021-08-12 09:31:10 -0300

    projeto criado

---
+++ src/main/java/com/valbersouza/cursojavaspringvr/CursojavaspringvrApplication.java
@@ -0,0 +1,13 @@
+package com.valbersouza.cursojavaspringvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursojavaspringvrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursojavaspringvrApplication.class, args);
+    }
+
+}
