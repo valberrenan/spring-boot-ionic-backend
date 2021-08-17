ed87c3e59f51ccc72fc38ea097522a538784087d
“Valber” <valber.lrenan@mercadolivre.com>
2021-08-17 08:49:06 -0300

    operação de instanciação

--- src/main/java/com/valbersouza/cursojavaspringvr/CursojavaspringvrApplication.java
+++ src/main/java/com/valbersouza/cursojavaspringvr/CursojavaspringvrApplication.java
@@ -1,13 +1,30 @@
 package com.valbersouza.cursojavaspringvr;
 
+import com.valbersouza.cursojavaspringvr.domain.Categoria;
+import com.valbersouza.cursojavaspringvr.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursojavaspringvrApplication {
+public class CursojavaspringvrApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursojavaspringvrApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritorio");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
