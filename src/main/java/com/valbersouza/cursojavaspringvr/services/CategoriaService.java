88b9eb891c8f381d397f884db2b2df0e4b8ea248
“Valber” <valber.lrenan@mercadolivre.com>
2021-08-19 22:11:32 -0300

    ajustes endpoint categorias /id

--- src/main/java/com/valbersouza/cursojavaspringvr/services/CategoriaService.java
+++ src/main/java/com/valbersouza/cursojavaspringvr/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.valbersouza.cursojavaspringvr.services;
 
 import com.valbersouza.cursojavaspringvr.domain.Categoria;
 import com.valbersouza.cursojavaspringvr.repositories.CategoriaRepository;
+import com.valbersouza.cursojavaspringvr.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id){
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + " Tipo:  " + Categoria.class.getName()));
     }
 }
