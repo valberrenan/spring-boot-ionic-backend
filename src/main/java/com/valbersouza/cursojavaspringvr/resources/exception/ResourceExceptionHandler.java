e91a5c9c69b7ec4c600fd24721b8b133640bac3e
“Valber” <valber.lrenan@mercadolivre.com>
2021-09-10 09:12:39 -0300

    Deletando uma categoria com Delete

--- src/main/java/com/valbersouza/cursojavaspringvr/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/valbersouza/cursojavaspringvr/resources/exception/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.valbersouza.cursojavaspringvr.resources.exception;
 
+import com.valbersouza.cursojavaspringvr.services.exceptions.DataIntegrityException;
 import com.valbersouza.cursojavaspringvr.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,5 +18,12 @@ public class ResourceExceptionHandler {
 
         StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegryti(DataIntegrityException e, HttpServletRequest request){
+
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
--- src/main/java/com/valbersouza/cursojavaspringvr/services/CategoriaService.java
+++ src/main/java/com/valbersouza/cursojavaspringvr/services/CategoriaService.java
@@ -2,8 +2,10 @@ package com.valbersouza.cursojavaspringvr.services;
 
 import com.valbersouza.cursojavaspringvr.domain.Categoria;
 import com.valbersouza.cursojavaspringvr.repositories.CategoriaRepository;
+import com.valbersouza.cursojavaspringvr.services.exceptions.DataIntegrityException;
 import com.valbersouza.cursojavaspringvr.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -28,5 +30,14 @@ public class CategoriaService {
     public Categoria update(Categoria obj) {
         find(obj.getId());
         return repo.save(obj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            repo.deleteById(id);
+        }catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos!!");
+        }
     }
 }
