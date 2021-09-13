18e07a878a911901f01644bce6e84990439157af
“Valber” <valber.lrenan@mercadolivre.com>
2021-09-13 20:31:54 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/valbersouza/cursojavaspringvr/resources/CategoriaResource.java
+++ src/main/java/com/valbersouza/cursojavaspringvr/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.valbersouza.cursojavaspringvr.resources;
 
 import com.valbersouza.cursojavaspringvr.domain.Categoria;
+import com.valbersouza.cursojavaspringvr.dto.CategoriaDTO;
 import com.valbersouza.cursojavaspringvr.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -41,5 +44,12 @@ public class CategoriaResource {
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
