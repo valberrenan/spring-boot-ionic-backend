2925b438814bad174b66b28750c48940d20b4001
“Valber” <valber.lrenan@mercadolivre.com>
2021-09-24 18:44:09 -0300

    validacao customizada email sem repeticao

--- src/main/java/com/valbersouza/cursojavaspringvr/repositories/ClienteRepository.java
+++ src/main/java/com/valbersouza/cursojavaspringvr/repositories/ClienteRepository.java
@@ -3,7 +3,12 @@ package com.valbersouza.cursojavaspringvr.repositories;
 import com.valbersouza.cursojavaspringvr.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    @Transactional(readOnly= true)
+    Cliente findByEmail(String email);
 }
--- src/main/java/com/valbersouza/cursojavaspringvr/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/valbersouza/cursojavaspringvr/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.valbersouza.cursojavaspringvr.services.validation;
 
+import com.valbersouza.cursojavaspringvr.domain.Cliente;
 import com.valbersouza.cursojavaspringvr.domain.enums.TipoCliente;
 import com.valbersouza.cursojavaspringvr.dto.ClienteNewDTO;
+import com.valbersouza.cursojavaspringvr.repositories.ClienteRepository;
 import com.valbersouza.cursojavaspringvr.resources.exception.FieldMessage;
 import com.valbersouza.cursojavaspringvr.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +14,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+   @Autowired
+   private ClienteRepository repo;
+
     @Override
     public void initialize(ClienteInsert ann) {
     }
@@ -25,6 +32,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if (aux != null) {
+            list.add(new FieldMessage("email", "Email já existente"));
         }
 
         for (FieldMessage e : list) {
