5d675b0dd4bc669aafd698f6adcb019edfcbe9ad
“Valber” <valber.lrenan@mercadolivre.com>
2021-09-26 14:59:48 -0300

    Validação customizada: email não repetido na atualização do cliente

--- src/main/java/com/valbersouza/cursojavaspringvr/dto/ClienteDTO.java
+++ src/main/java/com/valbersouza/cursojavaspringvr/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package com.valbersouza.cursojavaspringvr.dto;
 
 import com.valbersouza.cursojavaspringvr.domain.Cliente;
+import com.valbersouza.cursojavaspringvr.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
