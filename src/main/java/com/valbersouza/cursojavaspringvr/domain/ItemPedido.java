f5aba47d46b7e807bf89e14b9ec69c1ecfd96956
“Valber” <valber.lrenan@mercadolivre.com>
2021-10-19 19:02:08 -0300

    Implementando toString de Pedido

--- src/main/java/com/valbersouza/cursojavaspringvr/domain/ItemPedido.java
+++ src/main/java/com/valbersouza/cursojavaspringvr/domain/ItemPedido.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Objects;
 
 @Entity
@@ -96,4 +98,21 @@ public class ItemPedido implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    @Override
+    public String toString() {
+      NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+      StringBuilder builder = new StringBuilder();
+      builder.append(getProduto().getNome());
+      builder.append(", Qte: ");
+      builder.append(getQuantidade());
+      builder.append(", Preço unitário: ");
+      builder.append(nf.format(getPreco()));
+      builder.append(", Subtotal: ");
+      builder.append(nf.format(getSubTotal()));
+      builder.append("\n");
+      return builder.toString();
+    }
 }
+
+
