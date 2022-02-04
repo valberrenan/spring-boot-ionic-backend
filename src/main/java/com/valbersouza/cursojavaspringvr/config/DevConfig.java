8c3f2006f2d9f9ffab5399bc2c08cfb95d451296
“Valber” <valber.lrenan@mercadolivre.com>
2022-02-04 18:32:17 -0300

    ajustando perfil dev

--- src/main/java/com/valbersouza/cursojavaspringvr/config/DevConfig.java
+++ src/main/java/com/valbersouza/cursojavaspringvr/config/DevConfig.java
@@ -19,6 +19,9 @@ public class DevConfig {
     @Value("${spring.jpa.hibernate.ddl-auto}")
     private String strategy;
 
+    @Value("${spring.datasource.url}")
+    private String abc;
+
     @Bean
     public boolean instanciateDataBase() throws ParseException {
 
